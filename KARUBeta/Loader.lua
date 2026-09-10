72cda2799fa5e9a02f64c48aeb49fc4c97f9ee6d
KARUHUB <teerathepx2123@gmail.com>
2026-09-10 22:18:58 +0700

    Update Loader.lua

--- KARUBeta/Loader.lua
+++ KARUBeta/Loader.lua
@@ -1,1 +1,14 @@
+repeat task.wait() until game:IsLoaded()
 
+local GameId = game.GameId
+
+local SupportedGames = {
+    [1268927906] = "https://raw.githubusercontent.com/KARUscript/Loader/refs/heads/main/KARUBeta/ML.lua"
+}
+
+if SupportedGames[GameId] then
+    loadstring(game:HttpGet(SupportedGames[PlaceId]))()
+    warn("✅ Game supported")
+else
+    warn("❌ Game not supported")
+end
